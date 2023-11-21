7f55cfd9922bb29dc13ac67a9035de06e03ed461
Marleen <49445100+aMiyukii@users.noreply.github.com>
2023-11-21 10:01:25 +0100

    remove file upload

--- Sewapp/Controllers/AddPatternController.cs
+++ Sewapp/Controllers/AddPatternController.cs
@@ -12,28 +12,7 @@ namespace Sewapp.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddPattern(AddPatternViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (model.File != null && model.File.Length > 0)
-                {
-                    var filePath = Path.Combine("your_upload_directory", model.File.FileName);
-
-                    using (var stream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await model.File.CopyToAsync(stream);
-                    }
-                }
-
-                string patternName = model.PatternName;
-                string category = model.Category;
-
-                return RedirectToAction("Index");
-            }
-
-            return View(model);
-        }
+        
+        //database connectie hier
     }
 }
