0c30fa595a1424307f3d72288c17fe480e07896a
Marleen <49445100+aMiyukii@users.noreply.github.com>
2023-12-01 12:52:07 +0100

    Sending pattern with FormMethod.Post

---
+++ Sewapp/Controllers/PatternController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sewapp.Core.Models;
+using Sewapp.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Sewapp.Controllers
+{
+    public class PatternController : Controller
+	    {
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+        
+        public IActionResult AddPatternForm()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPatternName(Pattern pattern)
+        {
+                Console.WriteLine($"Pattern Name: {pattern.Name}");
+
+            return View("Index");
+        }
+    }
+
+}
+
